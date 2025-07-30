da81a63702a2821238280fc83ac78bc51b91068f
PHET45 <phnjk2000@gmail.com>
2025-07-30 13:45:19 +0700

    feat: add checkHeight function to handle undefined height

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
-
+const checkHeight = (height) => {
+    return height ?? "Height is not defined";
+};
+result = checkHeight(height)
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
